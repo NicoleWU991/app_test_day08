a6e2c4083a960d347e30800f8670dccfe51fdb9d
NicoleWU991 <hi_emily_wu@163.com>
2018-07-06 15:58:37 -0700

    'test_scripts'

---
+++ scripts/test_1.py
@@ -0,0 +1,7 @@
+class Test_c:
+    def test_01(self):
+        print('this is my first item')
+        assert True
+    def test_02(self):
+        print('this is second item')
+        assert False
