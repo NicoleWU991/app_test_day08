6a95ccef8a6277e3b939e01e605584d9c1230b11
NicoleWU991 <hi_emily_wu@163.com>
2018-07-06 19:36:13 -0700

    'success'

--- scripts/test_1.py
+++ scripts/test_1.py
@@ -1,7 +1,14 @@
+import allure
+import pytest
+
+
 class Test_c:
+    @allure.step(title='first test')
     def test_01(self):
+        allure.attach('这是描述','这是描述内容')
         print('this is my first item')
         assert True
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     def test_02(self):
         print('this is second item')
         assert False
